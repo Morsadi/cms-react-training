9a0a9588b72cc08522a14200cc3f655be90e8f16
Badr Morsadi <badreddine.morsadi@gmail.com>
2023-02-22 16:59:13 -0700

    Progress

--- comic/src/pages/api/comics/index.ts
+++ comic/src/pages/api/comics/index.ts
@@ -2,26 +2,31 @@
 import { NextApiResponse, NextApiRequest } from 'next';
 import md5 from 'md5';
 import { FetchedResult } from '../../../../types';
-
 const api_key = '3ddd7fcf726acc8fa2940749b2c8641d';
 const private_key = '6629ec43fdacee75cb0e6419ff2d3ea12e7334f6';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const headers: string[] = req.rawHeaders; 
+	
+	// Prevent endpoint direct access 
+	if (!headers.includes('Referer')) {
+		res.status(200).send({message: 'Restricted'});
+		return;
+	}
+
 	// const API_BASE = process.env.COMIC_API_BASE;
 	const baseUrl = 'http://gateway.marvel.com/v1/public/comics';
 	const ts = Date.now().toString();
 	const apiKey = api_key;
 	const privateKey = private_key;
-
 	const hash = md5(ts + privateKey + apiKey);
-
 	const api = `${baseUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}`;
-
+	
 	try {
 		const response = await fetch(api);
 		const data = await response.json();
 		const { results } = data.data;
-
+		
 		const newResults = results.map((comic: FetchedResult) => {
 			const {
 				id,
@@ -34,7 +39,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 			const thumbnail = `${img.path}.${img.extension}`;
 
 			return { id, thumbnail, creators, issueNumber, title, publishDate };
-			// return comic
 		});
 
 		res.status(200).json(newResults);
